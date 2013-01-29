092e7f3e871bd2b5e91123dac4b06200278da519
Robert J. Macomber <robert.macomber@socrata.com>
2013-01-29 09:30:08 -0800

    Forgot @deprecated's since parameter was introduced in 2.9

--- src/main/scala/com/socrata/soql/brita/Brita.scala
+++ src/main/scala/com/socrata/soql/brita/Brita.scala
@@ -1,6 +1,6 @@
 package com.socrata.soql.brita
 
-@deprecated("Use IdentifierFilter instead", "1.1.0")
+@deprecated("Use IdentifierFilter instead")
 object Brita {
   def apply(xs: Iterable[String]) = IdentifierFilter(xs)
   def apply(x: String) = IdentifierFilter(x)
--- src/main/scala/com/socrata/soql/brita/LegacyBrita.scala
+++ src/main/scala/com/socrata/soql/brita/LegacyBrita.scala
@@ -1,6 +1,6 @@
 package com.socrata.soql.brita
 
-@deprecated("Use LegacyIdentifierFilter instead", "1.1.0")
+@deprecated("Use LegacyIdentifierFilter instead")
 object LegacyBrita {
   def apply(xs: Iterable[String]) = LegacyIdentifierFilter(xs)
   def apply(x: String) = LegacyIdentifierFilter(x)
